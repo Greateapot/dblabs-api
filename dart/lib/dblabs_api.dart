b71d238a5b8aa8c2424f67505097fa628667ae64
Greateapot <greateapot998@gmail.com>
2023-11-05 18:30:21 +0500

    ddl fixes

--- dart/lib/dblabs_api.dart
+++ dart/lib/dblabs_api.dart
@@ -3,6 +3,7 @@ library dblabs_api;
 export 'src/generated/options.pb.dart';
 export 'src/generated/data_type.pb.dart';
 export 'src/generated/objects.pb.dart';
+export 'src/generated/any.pb.dart';
 export 'src/generated/enums.pb.dart';
 export 'src/generated/error.pb.dart';
 export 'src/generated/requests.pb.dart';
